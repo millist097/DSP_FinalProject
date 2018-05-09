256f6ceb192ee7889ec2c9016cee9ba3ffa729c1
millist.097@gmail.com <millist.097@gmail.com>
2018-05-09 10:29:44 -0500

    did dft filtering to remove dc and everything except the main frequency comonent

--- simpleDirection.m
+++ simpleDirection.m
@@ -10,6 +10,7 @@ clear M
 % M = csvread('459Hz_70d_70cm.csv');
 % M = csvread('459Hz_60d_70cm.csv');
 % M = csvread('459Hz_50d_70cm.csv');
+%M = csvread('dataTestLong2.csv');
 figure(1)
 stem(M(2:end,4))
 title('signal 4')
@@ -28,17 +29,17 @@ figure(2)
 stem(M(:,1))
 title('Signal 1')
 theta=0;
-b=2047;
+N=2047;
 for i = [0:4]
     
    % corrAD= xcorr(M(1+(b*i):b*(i+1),1),M(1+(b*i):b*(i+1),4));
 corrAD= xcorr((M(:,1)));
-corrAD(b) = corrAD(b)/9
-    [a,k] = max(abs(corrAD));
+corrAD(N) = corrAD(N)/9
+    [a4,k] = max(abs(corrAD));
     figure(3);
     stem(corrAD);
     title('corralation')
-    temp = acos(abs(-b+k)*343/(fs*d))
+    temp = acos(abs(-N+k)*343/(fs*d));
     theta = theta + temp;
 
 
@@ -52,15 +53,40 @@ temp*180/pi
 % lag = abs(N/2 - l);
 % 
 % asdf =acosd(l/d)
-% figure(4)
+figure(4)
 
-% aoeu = fft(M(:,4));
-% subplot(2,1,1)
-% 
-% stem(abs(aoeu(2:300)))
-% title('DFT')
-% subplot(2,1,2)
-% stem(phase(aoeu(2:300)))
+aoeu4 = fft(M(:,4));
+aoeu1 =fft(M(:,1));
+subplot(2,1,1)
+
+
+mag4 = abs(aoeu4);
+mag1 = abs(aoeu1);
+[a4,b4]=max(mag4  );
+[a1,b1] = max( mag1);
+mag4 = mag4.*.1;
+mag1 = mag1.*.1;
+mag4(b4)=a4;
+mag4(1)=0;
+mag1(b1)=a1;
+mag1(1)=0;
+clean4 = real(ifft(mag4.*exp(j*phase(aoeu4))));
+clean1 = real(ifft(mag1.*exp(j*phase(aoeu1))));
+figure(5)
+subplot(2,1,1)
+stem(clean1)
+subplot(2,1,2)
+stem(clean4)
+
+corrAD = xcorr(clean1(100:200),clean4(100:200));
+N=100
+figure(6)
+stem(corrAD)
+[a4,k] = max(corrAD);
+    figure(7);
+    stem(corrAD);
+    title('corralation')
+    temp = acos(abs(-N+k)*343/(fs*d))*180/pi
 % 
 % delay  =((-800+k)*343/fs);
 % (delay^2 + d^2)/(2*delay - 2*d*cos(pi/2-theta))
