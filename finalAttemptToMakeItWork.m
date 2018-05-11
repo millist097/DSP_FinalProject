6e5319c7b991f80ba9252255ca525b46ad3d686b
millist.097@gmail.com <millist.097@gmail.com>
2018-05-11 08:19:55 -0500

    added some pictures

--- finalAttemptToMakeItWork.m
+++ finalAttemptToMakeItWork.m
@@ -1,22 +1,22 @@
-[sig1 fs]= audioread('DataSets/duelChannelTest.wav');
+[sig1 fs]= audioread('DataSets/threeHellos_andaclap.wav');
 
 left = sig1(:,1)/max(sig1(:,1));
 right = sig1(:,2)/max(sig1(:,2));
-
-for i = 10:length(left)
-    left(i) = mean(left(i-9:i));
-    right(i) = mean(left(i-9:i));
-end
 
 
 t = [1:length(left)]/fs;
 
 plot(t,left)
 
-shortL_1 = left(round(.79*fs):round(1*fs)-1);
-shortR_1 = right(round(.79*fs):round(1*fs)-1);
-shortR_2 = right(round(3.5*fs):round(3.89*fs)-1);
-shortL_2 = left(round(3.5*fs):round(3.89*fs)-1);
+shortL_3 = left(round(5.106*fs):round(5.393*fs)-1);
+shortR_3 = right(round(5.106*fs):round(5.393*fs)-1);
+shortR_clap = right(round(9.89*fs):round(10.13*fs)-1);
+shortL_clap = left(round(9.89*fs):round(10.13*fs)-1);
+shortL_1 = left(round(.879*fs):round(1.184*fs)-1);
+shortR_1 = right(round(.879*fs):round(1.184*fs)-1);
+shortL_2 = left(round(3.102*fs):round(3.412*fs)-1);
+shortR_2 = right(round(3.102*fs):round(3.412*fs)-1);
+
 
 figure(2)
 subplot(2,1,1)
@@ -24,17 +24,21 @@ plot(shortL_1)
 subplot(2,1,2)
 plot(shortR_1)
 angle = 0;
-d=3*.0475;
-for i =1:17
-    corrLR = xcorr(shortR_2((i-1)*500+1:i*500),...
-                   shortL_2((i-1)*500+1:i*500));
+d=3*.035;
+figure(3)
+k = 50;
+
+for i =1:200
+    corrLR = xcorr(shortL_clap((i-1)*k+1:i*k),...
+                   shortR_clap((i-1)*k+1:i*k));
     plot(corrLR)
+    
     %pause
-    [a,n]=max(corrLR);
-    n = n-500;
-    temp = acos(abs(n)*343/(fs*d))*180/pi
+    [a,n(i)]=max(corrLR);
+    n(i) = n(i)-k;
+    temp = acos(abs(n(i))*343/(fs*d))*180/pi
     angle = angle + temp;
 end
 
 
-angle/17
+angle/200
